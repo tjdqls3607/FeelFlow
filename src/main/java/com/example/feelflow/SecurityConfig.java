eae58f50f15c7be70487f08ff37d6fd2a369314f
정성빈 <tjdqls3124@gmail.com>
2024-09-13 16:45:35 +0900

    로그인 및 세션 해결중

--- src/main/java/com/example/feelflow/SecurityConfig.java
+++ src/main/java/com/example/feelflow/SecurityConfig.java
@@ -18,15 +18,28 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(new AntPathRequestMatcher("/user/login"),
+                                new AntPathRequestMatcher("/user/signup"),
+                                new AntPathRequestMatcher("/user/logout"),
+                                new AntPathRequestMatcher("/main"),
+                                new AntPathRequestMatcher("/diary/**"),  // 일기 관련 경로를 추가할 수 있습니다.
+                                new AntPathRequestMatcher("/**")).permitAll() // 모든 경로 접근 허용
+                )
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
 
                 .formLogin((formLogin) -> formLogin
                         .loginPage("/user/login")
-                        .defaultSuccessUrl("/"))
+                        .defaultSuccessUrl("/main.html", true))
+
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/user/login") // 로그아웃 후 로그인 페이지로 리디렉트
+                        .invalidateHttpSession(true) // 세션 무효화
+                        .deleteCookies("JSESSIONID") // 세션 쿠키 삭제
+                );
                 ;
         return http.build();
 
